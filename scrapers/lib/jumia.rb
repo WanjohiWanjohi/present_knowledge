3cd2e6a010a07e62335fd6120a0c9cddede58c53
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-09-08 11:48:13 +0300

    Begin Jumia scraper

--- scrapers/lib/jumia.rb
+++ scrapers/lib/jumia.rb
@@ -1,15 +1,46 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
-def get_page
-    page = Nokogiri::HTML(open('https://www.jumia.co.ke/'))
-end
+class JumiaScraper
+    attr_accessor :urls
+    DOMAIN_NAME = 'https://www.jumia.co.ke'
+    
+    def get_page
+        page = Nokogiri::HTML(open(DOMAIN_NAME))
+    end
 
-def get_categories
-    category_names = []
-    category_urls = []
-    all_categories = page_get_page.css("a[role='menuitem']")
-    all_categories.each do | category|
-        
+    def get_categories
+        categories ={}
+        all_categories = get_page.css("a[role='menuitem']")
+        all_categories.each do | category|
+            categories[category.text.to_sym ] = category["href"]
+        end
+        @urls = categories
+        return categories
+    end
+
+    def get_category_urls
+        get_categories.each do | category|
+            category_name = category[0].to_s
+            category_url = construct_url(category[1].to_s)
+            parse_category_products(url, category_name)
+            binding.pry
+        end
+    end
+
+    def parse_category_products(url, category_name)
+
+    end
+
+    def construct_url(url)
+        if url.start_with?("https")
+            return url
+        else
+            return DOMAIN_NAME + url
+        end
     end
 end
+
+jum = JumiaScraper.new()
+jum.get_category_urls
