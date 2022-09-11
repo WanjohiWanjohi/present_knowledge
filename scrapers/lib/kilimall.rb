1133e2c1b1817312ef65b114c3487ed0e24f32df
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-09-11 19:21:15 +0300

    Wire up initial scraper classes and login page

--- scrapers/lib/kilimall.rb
+++ scrapers/lib/kilimall.rb
@@ -1,1 +1,36 @@
 require 'nokogiri'
+require 'open-uri'
+require 'pry'
+# require_relative '../app/models/product'
+
+class JumiaScraper
+    attr_accessor :urls
+    vendor = "Kilimall"
+    DOMAIN_NAME = 'https://www.kilimall.co.ke/new/'
+    
+    def get_page
+        page = Nokogiri::HTML(open(DOMAIN_NAME))
+    end
+
+    def get_categories
+    end
+
+    def get_category_urls
+    end
+
+    def parse_category_products
+    end
+    
+    def parse_product_image
+    end
+    
+    def parse_product_info
+    end
+    
+    
+    def construct_url
+    end
+end
+
+kil = KilimallScraper.new()
+kil.get_category_urls
--- scrapers/lib/skygarden.rb
+++ scrapers/lib/skygarden.rb
@@ -1,1 +1,36 @@
 require 'nokogiri'
+require 'open-uri'
+require 'pry'
+# require_relative '../app/models/product'
+
+class JumiaScraper
+    attr_accessor :urls
+    vendor = "Sky Garden"
+    DOMAIN_NAME = 'https://sky.garden/home'
+    
+    def get_page
+        page = Nokogiri::HTML(open(DOMAIN_NAME))
+    end
+
+    def get_categories
+    end
+
+    def get_category_urls
+    end
+
+    def parse_category_products
+    end
+    
+    def parse_product_image
+    end
+    
+    def parse_product_info
+    end
+    
+    
+    def construct_url
+    end
+end
+
+jum = JumiaScraper.new()
+jum.get_category_urls
